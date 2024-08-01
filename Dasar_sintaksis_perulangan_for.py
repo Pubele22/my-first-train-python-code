c73f7c67847f027c231f9c7c8fab7e564a17d073
yusti <yustinokwek22@gmail.com>
2024-08-02 02:34:42 +0700

    Perulangan

---
+++ Dasar_sintaksis_perulangan_for.py
@@ -0,0 +1,13 @@
+"""
+program perulangan membaca buku
+"""
+
+jumlah_buku = 20
+print('perintah ibu "baca semua bukumu"')
+
+jumlah_buku_terbaca = 0
+print(f"jumlah buku terbaca {jumlah_buku_terbaca}")
+for jumlah_buku_terbaca in range(1, jumlah_buku + 1):
+    print(f"buku ke {jumlah_buku_terbaca} sudah dibaca")
+
+print(f"jumlah buku terbaca {jumlah_buku_terbaca}")
